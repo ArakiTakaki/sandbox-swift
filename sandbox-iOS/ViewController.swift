0b9f6d762d737e8f09af43e4852844647dfaafca
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-01-18 22:16:36 +0900

    Initial Commit

---
+++ sandbox-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sandbox-iOS
+//
+//  Created by 荒木駿輝 on 2019/01/18.
+//  Copyright © 2019 TakakiAraki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ sandbox-iOSTests/sandbox_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  sandbox_iOSTests.swift
+//  sandbox-iOSTests
+//
+//  Created by 荒木駿輝 on 2019/01/18.
+//  Copyright © 2019 TakakiAraki. All rights reserved.
+//
+
+import XCTest
+@testable import sandbox_iOS
+
+class sandbox_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
